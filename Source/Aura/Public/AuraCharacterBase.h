54df4aecb29e35149f3d62c5adc166b1f77e2f23
성민 류 <baekhun0330@naver.com>
2025-08-24 21:10:47 +0900

    클래스 추가

--- Source/Aura/Public/AuraCharacterBase.h
+++ Source/Aura/Public/AuraCharacterBase.h
@@ -15,6 +15,7 @@ public:
 	// Sets default values for this character's properties
 	AAuraCharacterBase();
 
+	
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
